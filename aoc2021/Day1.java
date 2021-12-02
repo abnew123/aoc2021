f06da0f9ee4abf4cbea0562b97c82cdb153067e1
abnew123 <shadowghosts31415@gmail.com>
2021-12-02 00:08:18 -0500

    Day1 optimizations

--- aoc2021/Day1.java
+++ aoc2021/Day1.java
@@ -13,26 +13,14 @@ public class Day1 {
 		File file = new File("./aoc2021/day" + day + ".txt");
 		Scanner in = new Scanner(file);
 		int answer = 0;
+		int offset = part1?1:3; // a+b+c < b+c+d iff a <d
 		List<Integer> nums = new ArrayList<>();
-		List<Integer> nums2 = new ArrayList<>();
 		while(in.hasNext()) {
 			nums.add(in.nextInt());
 		}
-		for(int i = 0; i < nums.size()-2; i++) {
-			nums2.add(nums.get(i) + nums.get(i+1) + nums.get(i+2));
-		}
-		if(part1) {
-			for(int i = 1; i < nums.size(); i++) {
-				if(nums.get(i) > nums.get(i-1)) {
-					answer+=1;
-				}
-			}
-		}
-		else {
-			for(int i = 1; i < nums2.size(); i++) {
-				if(nums2.get(i) > nums2.get(i-1)) {
-					answer+=1;
-				}
+		for(int i = 0; i < nums.size() - offset; i++) {
+			if(nums.get(i+offset) > nums.get(i)) {
+				answer+=1;
 			}
 		}
 		in.close();
